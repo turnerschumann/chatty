8e45c0db91484da54560b5cac69f19acac382ae6
Turner Schumann <turnerschumann@gmail.com>
2019-06-04 18:51:39 +0000

    Add chatbar and message components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import Chatbar from './Chatbar.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <Message />
+        <Chatbar currentUser={this.state.currentUser}/>
+      </div>
     );
   }
 }

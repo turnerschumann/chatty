d47c4df81e2aee5936f42c291eebb41fa8e40caf
Turner Schumann <turnerschumann@gmail.com>
2019-06-04 20:55:25 +0000

    Implement messagelist.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
 import Chatbar from './Chatbar.jsx';
 
 class App extends Component {
@@ -13,14 +14,43 @@ class App extends Component {
         {
           username: "Bob",
           content: "Has anyone seen my marbles?",
+          id: 1
         },
         {
           username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: 2
         }
       ]
     }
   }
+
+  onCompose(event) {
+    this.setState((prev, props) => ({
+      const messageObj = {
+        username: this.state.currentUser.name,
+        content: event.target.value,
+        id 4:
+      }
+      messages : messages.push(event.target.value)
+
+    }));
+  }
+
+
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+}
 
   render() {
     return (
@@ -30,6 +60,7 @@ class App extends Component {
         </nav>
         <Message />
         <Chatbar currentUser={this.state.currentUser}/>
+        <MessageList messages={this.state.messages}/>
       </div>
     );
   }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,35 +1,89 @@
-state.messages = [
-  {
-    type: "incomingMessage",
-    content: "I won't be impressed with technology until I can download food.",
-    username: "Anonymous1"
-  },
-  {
-    type: "incomingNotification",
-    content: "Anonymous1 changed their name to nomnom",
-  },
-  {
-    type: "incomingMessage",
-    content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-    username: "Anonymous2"
-  },
-  {
-    type: "incomingMessage",
-    content: "...",
-    username: "nomnom"
-  },
-  {
-    type: "incomingMessage",
-    content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-    username: "Anonymous2"
-  },
-  {
-    type: "incomingMessage",
-    content: "This isn't funny. You're not funny",
-    username: "nomnom"
-  },
-  {
-    type: "incomingNotification",
-    content: "Anonymous2 changed their name to NotFunny",
-  },
-]
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+// function MessageList(props) {
+
+//   const messages = props.messages};
+//   const messageItems = messages.map((message) =>
+//     // return <Message message={message}/>
+//   );
+// }
+
+
+// class MessageList extends Component {
+
+//   const messages = {this.props.messages};
+//   const messageItems = messages.map((message) =>
+//     // return <Message message={message}/>
+//   );
+
+//   render() {
+//     return (
+//       <ul>{messageItems}</ul>
+//     );
+//   }
+
+// }
+
+class MessageList extends Component {
+  render() {
+    const messages = this.props.messages.map(message => {
+      return <Message
+        key={ message.id }
+        username={ message.username }
+        content={ message.content }
+      />
+    });
+
+    return (
+      <section className="messages">
+        { messages }
+      </section>
+    )
+  }
+}
+
+export default MessageList;
+
+
+
+
+
+
+
+// state.messages = [
+//   {
+//     type: "incomingMessage",
+//     content: "I won't be impressed with technology until I can download food.",
+//     username: "Anonymous1"
+//   },
+//   {
+//     type: "incomingNotification",
+//     content: "Anonymous1 changed their name to nomnom",
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+//     username: "Anonymous2"
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "...",
+//     username: "nomnom"
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+//     username: "Anonymous2"
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "This isn't funny. You're not funny",
+//     username: "nomnom"
+//   },
+//   {
+//     type: "incomingNotification",
+//     content: "Anonymous2 changed their name to NotFunny",
+//   },
+// ]
+
